b3ae084b24d456a0d7f71a94880dfe4fe30507ab
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-09-20 02:02:10 +0530

    Update usePagination.jsx

--- src/hooks/usePagination.jsx
+++ src/hooks/usePagination.jsx
@@ -1,1 +1,25 @@
+import { useState } from "react";
 
+export const usePagination = () => {
+    const [page, setPage] = useState(1);
+    const [rows, setRows] = useState(10);
+    const [total, setTotal] = useState(1);
+
+    const goToNextPage = () => {
+        setPage(prev => Math.min(prev + 1, total))
+    }
+
+    const goToPrevPage = () => {
+            setPage(prev => Math.max(prev-1, 1))
+    }
+
+    const updateTotal = (total) => {
+        setTotal(total)
+    }
+
+    const hasNextPage = ()=>{
+        return page < total
+    }
+
+    return { page, rows, total, goToNextPage, goToPrevPage, updateTotal, hasNextPage }
+}
