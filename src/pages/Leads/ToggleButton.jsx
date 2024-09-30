bc0aadb141bde57f8de2b8bd93b8c6689a62e118
Nitian Bit <155094550+nitianbit@users.noreply.github.com>
2024-10-01 00:11:16 +0530

    Update ToggleButton.jsx

--- src/pages/Leads/ToggleButton.jsx
+++ src/pages/Leads/ToggleButton.jsx
@@ -7,21 +7,22 @@ const ToggleButton = ({formType, setFormType}) => {
     return (
         <div className="container mt-4">
             <div className="btn-group btn-group-toggle" data-bs-toggle="buttons">
-                <label className={`btn btn-secondary toggle-btn ${formType === "contact" ? "active" : ""}`}>
-                    <input type="radio" checked={formType === "contact"} autocomplete="off" onClick={()=>{
-                        onToggle("contact")
-                    }}/> Contact
+            <label className={`btn btn-secondary toggle-btn ${formType === "product" ? "active" : ""}`}>
+                    <input type="radio" checked={formType === "product"} autocomplete="off" onClick={()=>{
+                        onToggle("product")
+                    }}/> Product
                 </label>
                 <label className={`btn btn-secondary toggle-btn ${formType === "client" ? "active" : ""}`}>
                     <input type="radio" checked={formType === "client"} autocomplete="off" onClick={()=>{
                         onToggle("client")
                     }}/> Client
                 </label>
-                <label className={`btn btn-secondary toggle-btn ${formType === "product" ? "active" : ""}`}>
-                    <input type="radio" checked={formType === "product"} autocomplete="off" onClick={()=>{
-                        onToggle("product")
-                    }}/> Product
+                <label className={`btn btn-secondary toggle-btn ${formType === "contact" ? "active" : ""}`}>
+                    <input type="radio" checked={formType === "contact"} autocomplete="off" onClick={()=>{
+                        onToggle("contact")
+                    }}/> Contact
                 </label>
+                
             </div>
         </div>
     )
